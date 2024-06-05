288a3580499e2345221c7f67a010e04fdd508e54
Vineet Thutheja <vthutheja@gmail.com>
2024-06-05 22:27:32 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Express } from 'express';
+import { GatewayServer } from '@gateway/server';
+
+class Application {
+    /**
+     * initialize
+     */
+    public initialize() {
+        const app: Express = express();
+        const server:GatewayServer = new GatewayServer(app);
+        server.start();
+
+    }
+
+    
+}
+
+const application: Application = new Application();
+application.initialize();
